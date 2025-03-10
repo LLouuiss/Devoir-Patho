2a6c17c4c7d1009e28deba3d1c078627361cbfa1
lherphelin <louis.herphelin@student.uclouvain.be>
2025-03-10 14:07:15 +0100

    add moment

--- devoir.py
+++ devoir.py
@@ -141,35 +141,33 @@ def V_rd(As_s,fywd,d,cot_theta):
     cot_theta : inclinaison bielle comprimée 
     d : distance entre armature béton exterieur (h-e) [m]
     """
-    z = 0.9 * d # Méthode simplifiée distance entre armature et résultante force béton comprimé [m]
+    z = 0.9 * d  # Méthode simplifiée distance entre armature et résultante force béton comprimé [m]
     V_rd = As_s*fywd*z*cot_theta # [N]
     return V_rd
 
 def M_rd(As, fyd,d):
     """ Fonction qui calcule le moment résistant de la section
-    d : distance entre armature béton exterieur (h-e) [m]
-    As : aire d'armature [mm^2/m]
+    d : distance entre armature béton exterieur (h-e) [mm]
+    As : aire d'armature [mm^2]
     fyd : limite élasticité acier avec coefficent sécurité [MPa]
     """
-    z = 0.9 * d # [m] méthode simplifiée
+    z = 0.9 * d # [mm] méthode simplifiée
     F_s = As * fyd # [N]
-    M_rd = F_s * z #[Nm]
+    M_rd = F_s * z #[Nmm]
     return M_rd
 
 def calcul_caracteristiques_section_non_fissuree(b1, h1,b2,h2,b3,h3, d, d_prime, A_s, A_s_prime, E_cm, phi_eff, E_s):
     """
     Calcule les caractéristiques mécaniques de la section non fissurée (y_G,I et I_I)
-    
-    :param b1: Largeur de la section en fibre inférieure  (m)
-    :param h1: Hauteur totale de la section (m)
-    :param d: Hauteur utile (m)
-    :param d_prime: Hauteur des armatures comprimées (m)
-    :param A_s: Aire des armatures tendues (m^2)
-    :param A_s_prime: Aire des armatures comprimées (m^2)
-    :param E_cm: Module d'élasticité du béton (GPa)
-    :param phi_eff: Coefficient de fluage effectif
-    :param E_s: Module d'élasticité de l'acier (GPa)
-    :return: y_G,I (m), I_I (m^4), alpha_e ()
+    b1,h1,b2,h2,b3,h3 ;  géométrie de la section (mm)
+    d : Hauteur armature tendue (mm)
+    d_prime : Hauteur des armatures comprimées (mm)
+    A_s : Aire des armatures tendues (mm^2)
+    A_s_prime : Aire des armatures comprimées (mm^2)
+    E_cm : Module d'élasticité du béton (MPa)
+    phi_eff : Coefficient de fluage effectif du béton
+    E_s : Module d'élasticité de l'acier (MPa)
+    return : y_G_I (mm), I_I (mm^4) et alpha_e
     """
     # Calcul du module d'élasticité effectif du béton
     E_c_eff = E_cm / (1 + phi_eff)  # GPa
@@ -178,57 +176,59 @@ def calcul_caracteristiques_section_non_fissuree(b1, h1,b2,h2,b3,h3, d, d_prime,
     alpha_e = E_s / E_c_eff
    
     # Calcul du centre de gravité de la section non fissurée
-    
-    y_G_I = (b1*h1*h1/2+b2*h2*(h1+h2/2)+b3*h3*(h1+h2+h3/2) + alpha_e * A_s * d + alpha_e * A_s_prime * d_prime) / (b1 * h1+b2*h2+b3*h3 + alpha_e * A_s + alpha_e * A_s_prime)
+    # Réference = bas de la poutre
+    y_G_I = (b1*h1*h1/2+b2*h2*(h1+h2/2)+b3*h3*(h1+h2+h3/2) + alpha_e * A_s * d + alpha_e * A_s_prime * d_prime) / (b1 * h1 + b2*h2 + b3*h3 + alpha_e * A_s + alpha_e * A_s_prime)
     
     # Calcul de l'inertie de la section non fissurée
     I_I = (b1 * h1**3 / 12) +(b2 * h2**3 / 12) +(b3 * h3**3 / 12) + b1 * h1 * (h1/2 - y_G_I)**2+b2 * h2 * (h2/2+h1 - y_G_I)**2+b3 * h3 * (h3/2+h2+h1 - y_G_I)**2 + alpha_e * A_s * (y_G_I - d)**2 + alpha_e * A_s_prime * (y_G_I - d_prime)**2
     
     return y_G_I, I_I,alpha_e
 
-def calcul_caracteristiques_section_fissuree(b1, h1,b2,h2,x, d, d_prime, A_s, A_s_prime, alpha_e):
+def calcul_caracteristiques_section_fissuree(b1, h1,b2,h2, d, d_prime, A_s, A_s_prime, alpha_e):
     """
     Calcule les caractéristiques mécaniques de la section fissurée (y_G,II et I_II)
-    
-    :param b: Largeur de la section (m)
-    :param x: Position de l'axe neutre (m)
-    :param d: Hauteur utile (m)
-    :param d_prime: Hauteur des armatures comprimées (m)
-    :param A_s: Aire des armatures tendues (mm^2)
-    :param A_s_prime: Aire des armatures comprimées (mm^2)
-    :param alpha_e: Coefficient d'équivalence acier/béton
-    :return: y_G_II (m), I_II (m^4)
+    b1,h1,b2,h2 ;  géométrie de la section (mm) là ou c'est pas fissuré
+    d : Hauteur armature tendue (mm)
+    d_prime : Hauteur des armatures comprimées (mm)
+    A_s : Aire des armatures tendues (mm^2)
+    A_s_prime : Aire des armatures comprimées (mm^2)
+    alpha_e : coefficient d'équivalence
+    return : y_G_II (mm) et I_II (mm^4) par rapport au bas/haut de h1
     """
-    
+    # Valeur initiale de x
+    x = h1
     for i in range(10):
-        x =  (b1 * x**2/2 + alpha_e * A_s * d + alpha_e * A_s_prime * d_prime) / (b1 *x + alpha_e * A_s + alpha_e * A_s_prime)
+        # Hypothèse position de l'axe neutre est dans la semelle
+        #x =  ((b1 * x**2)/2 + alpha_e * A_s * d + alpha_e * A_s_prime * d_prime) / (b1 *x + alpha_e * A_s + alpha_e * A_s_prime)
+        # Hypothèse position de l'axe neutre est dans l'âme
+        x =  (((b1 * h1**2)/2 + b2 * (x-h1) * x) + alpha_e * A_s * d + alpha_e * A_s_prime * d_prime) / (b1 * h1 + b2 * (x-h1) + alpha_e * A_s + alpha_e * A_s_prime)
     # Calcul de l'inertie de la section fissurée
-    x = 0.88
-    
-    I_II = (b1 * h1**3 / 12) +(b2 * (x-h1)**3 / 12) + b1 * h1 * (h1/2 - x)**2+b2 * (x-h1) * (h2/2+h1 - x)**2 + alpha_e * A_s * (x - d)**2 + alpha_e * A_s_prime * (x - d_prime)**2
+    # Hypothèse position de l'axe neutre est dans l'âme
+    I_II = (b1 * h1**3 / 12) +(b2 * (x-h1)**3 / 12) + b1 * h1 * (h1/2 - x)**2 + b2 * (x-h1) * ((x-h1)/2+h1 - x)**2 + alpha_e * A_s * (x - d)**2 + alpha_e * A_s_prime * (x - d_prime)**2
     return x, I_II
 
 
 def calcul_ouverture_fissures(M_II, d, x, I_II, A_s, b, h, E_s, alpha_e, k_t, f_ctm,c,k1,k2,phi) :
     """Calcule l'ouverture des fissures à l'ELS quasi-permanent
-    :param M_II: Moment fléchissant (kNm)
-    :param d: Hauteur utile (m)
-    :param x: Position de la fibre neutre (m)
-    :param I_II: Moment d'inertie (m^4)
-    :param A_s: Aire des armatures (mm^2)
-    :param b: Largeur de la section (mm)
-    :param h: Hauteur totale de la section (mm)
-    :param E_s: Module d'élasticité de l'acier (MPa)
-    :param alpha_e: Facteur de correction
-    :param k_t: Facteur de durée de charge
-    :param f_ctm: Résistance en traction du béton (MPa)
-    :param c: enrobage des armatures (m)
-    :param k1: Paramètre de fissuration
-    :param k2: Paramètre de fissuration
-    :param phi: Diamètre des armatures (mm)
-    :return: Ouverture des fissures W_k (mm)
+    M_II: Moment fléchissant (Nmm)
+    d : hauteur armature tendue (mm)
+    x: Position de la fibre neutre (mm)
+    I_II: Moment d'inertie section fissurée (mm^4)
+    A_s: Aire des armatures tendue (mm^2)
+    b: Largeur de la section (mm)
+    h: Hauteur totale de la section (mm)
+    E_s: Module d'élasticité de l'acier (MPa)
+    alpha_e: coefficient d'équivalence
+    k_t: Facteur de durée de charge
+    f_ctm: Résistance en traction du béton (MPa)
+    c: enrobage des armatures (mm)
+    k1: Paramètre de fissuration
+    k2: Paramètre de fissuration
+    phi: Diamètre des armatures (mm)
+    
     """
     
+    ### d et x doivent avoir meme ref
     
     # Calcul de la contrainte dans les armatures tendues (sigma_s)
     sigma_s = alpha_e * (M_II / I_II) * (d - x)  # MPa
@@ -236,9 +236,9 @@ def calcul_ouverture_fissures(M_II, d, x, I_II, A_s, b, h, E_s, alpha_e, k_t, f_
     print("x = ",x)
     print("sigma_s = ",sigma_s)
     # Calcul de la hauteur efficace du béton autour des armatures
-    h_ceff = 2.5 * (h - d)  # mm
+    h_ceff = min(2.5 * (h - d),(h-x)/3, h/2 ) # mm
     print("h_ceff = ",h_ceff)
-    # Calcul du taux d'armatures efficaces
+    ##### TO EDIT #####
     rho_seff = A_s / (h_ceff * b)
     print("rho_seff = ",rho_seff)
     # Calcul de la déformation epsilon_sm - epsilon_cm
@@ -251,12 +251,12 @@ def calcul_ouverture_fissures(M_II, d, x, I_II, A_s, b, h, E_s, alpha_e, k_t, f_
     print("epsilon_sm_cm = ",epsilon_sm_cm)
     
     # Espacement des fissures (approximé selon des formules usuelles)
-    s_r_max = 3.4*c + 0.425*k1*k2*phi/rho_seff  # Approche simplifiée
+    s_r_max = 3.4*c + 0.425*k1*k2*phi/rho_seff  # Approche simplifiée (mm)
     print("s_r_max = ",s_r_max)
     # Ouverture des fissures
-    W_k = s_r_max * epsilon_sm_cm  # mm
+    w_k = s_r_max * epsilon_sm_cm  # mm
     
-    return W_k*1000, s_r_max*1000
+    return w_k, s_r_max
 
 
 if __name__ == "__main__" :
@@ -271,38 +271,47 @@ if __name__ == "__main__" :
     Dce = Dce * 365*24*60*60 # [mm^2/ans]
     Cs = 0.15 # [%] concentration en surface
     
-    """Géométrie de la section"""
-    b1 = 0.8  # m
-    h1 = 0.3  # m
-    b2 = 0.4
-    h2 = 1.4
-    b3 = 1.4
-    h3 = 0.3
-    d = 1.85  # m
-    d_prime = 0.15  # m
-    A_s = 225.12e-4  # cm^2 28phi32
-    A_s_prime = 44.19e-4  # m^2 9phi25
+    e = 50 # [mm] enrobage
+    """Géométrie de la section au nu de la colonne """
+    b1 = 800 # mm
+    h1 = 300 # mm  
+    b2 = 400 # mm 
+    h2 = 1400 # mm
+    b3 = 1400 # mm
+    h3 = 300 # mm
+    
+    d = 2000 - e  # mm
+    d_prime = e # mm
+    A_s =  28 * 32**2 * np.pi/4 # mm^2 28phi32 en traction
+    print("A_s = ",A_s)
+    A_s_prime = 9 * 25**2 * np.pi/4  # mm^2 9phi25 en compression
     E_cm = 35.2e9  # Pa béton C40/50
-    phi_eff = 2
+    E_cm = E_cm * 1e-6 # MPa
+    phi_eff = 2 # coefficient de fluage effectif du béton
     E_s = 200e9  # Pa
+    E_s = E_s * 1e-6 # MPa
+    k_t = 0.4 # dans le cas d un chargement de longue durée
+    f_ctm = 3.51e6 # Pa beton C40/50
+    f_ctm = f_ctm * 1e-6 # MPa
+    k1 = 0.8 # pour des barres à haute adhérence
+    k2 = 0.5 # en flexion simple
+
+    
+    # Efforts sollicitants
     M = 7920000  # Nm
-    k_t = 0.4 #dans le cas d un chargement de longue durée
-    f_ctm = 3.51e6 #Pa beton C40/50
-    e = 0.05 #m enrobage des armatures
-    k1 = 0.8 #pour des barres à haute adhérence
-    k2 = 0.5 #en flexion simple
-    phi = 0.032 #m diamètre des armatures
-        
-    y_G_I, I_I,alpha_e = calcul_caracteristiques_section_non_fissuree(b1, h1,b2,h2,b3,h3, d, d_prime, A_s, A_s_prime, E_cm, phi_eff, E_s)
-    y_G_II, I_II = calcul_caracteristiques_section_fissuree(b1, h1,b2,h2,0.2, d, d_prime, A_s, A_s_prime, alpha_e)          
+    M = M * 1e3 # Nmm
+    
+    y_G_I, I_I, alpha_e = calcul_caracteristiques_section_non_fissuree(b1, h1,b2,h2,b3,h3, d, d_prime, A_s, A_s_prime, E_cm, phi_eff, E_s)
+    y_G_II, I_II = calcul_caracteristiques_section_fissuree(b1, h1,b2,h2, d, d_prime, A_s, A_s_prime, alpha_e)
+    phi = 32 # mm diamètre des armatures en traction        
     w, Sm0 = calcul_ouverture_fissures(M, d, y_G_II, I_II, A_s, b3, h1+h2+h3, E_s, alpha_e,k_t,f_ctm,e,k1,k2,phi)
     print("w [mm], si <0.3 -> OK = ",w)
     
-    e = 50 # [mm] enrobage
     t_compact1, t_fissure1 = find_ti_carbonatation(K,e,w)
     t_compact2, t_fissure2 = find_ti_corrosion(e,Dce,Clim,Cs,w,Sm0)
     
     print("Temps d'initiation de la corrosion : ",min(t_compact1,t_compact2,t_fissure1,t_fissure2)," ans")
+    
     
     vcorr_c = 2 # [microm/ans] si seulement carbonatation
     vcorr_c = vcorr_c * 1e-3 # [mm/ans]
@@ -311,20 +320,33 @@ if __name__ == "__main__" :
     t0 = min(t_compact1,t_compact2,t_fissure1,t_fissure2)
     
     # Plot de la réduction de la section d'armature pour ferraillage logitudinal et ferraillage étrier
-    Ns_L = 17 # nombre de barres d'armature longitudinales
-    d0_L = 40 # [mm] diamètre des barres d'armature longitudinales
+    Ns_L = 28 # nombre de barres d'armature longitudinales
+    d0_L = 32 # [mm] diamètre des barres d'armature longitudinales
     As_L = As_reduction(Ns_L,d0_L,min(t_compact1,t_fissure1),min(t_compact2,t_fissure2),t,vcorr_c,vcorr_cc)
     
-    #### ATTENTION POUR ETRIER ON VA DEVOIR DIVISER AS EN DEUX CAR FCT PREND EN COMPTE SEULEMENT UNE SECTION D'ARMATURE ####
-    e= 50 # [mm] enrobage
     fywd = 435 # [MPa] pour acier BE500S
+    M_rd_values = M_rd(As_L,fywd,d)
+    plt.plot(t,M_rd_values,label="Moment résistant")
+    plt.plot(t,M*np.ones(len(t)),'--',color = 'black',label="Moment sollicitant")
+    plt.xlabel("Temps [ans]")
+    plt.ylabel("Moment résistant [Nmm]")
+    plt.title("Moment résistant en fonction du temps pour un enrobage de" + str(e) + " mm")
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
+
+    
+    #### ATTENTION POUR ETRIER ON VA DEVOIR DIVISER AS EN DEUX CAR FCT PREND EN COMPTE SEULEMENT UNE SECTION D'ARMATURE ####
+    
+    ## On analyse la resistence des etriers pour la zone au nu de la colonne car effort tranchant plus critique
+    
     cot_theta = 2 # inclinaison bielle comprimée
-    d = 2 - e*(10**-3) # [m] distance entre armature béton extérieur ZONE 2 => 2 m
     Ns_E = 2*2 # 2 etriers de 2 barres
     d0_E = 11 # [mm] diamètre des barres d'armature étriers
     As_E = As_reduction(Ns_E,d0_E,min(t_compact1,t_fissure1),min(t_compact2,t_fissure2),t,vcorr_c,vcorr_cc)
     As_Es = As_E / 0.11 # [mm^2/m] 2 etriers tout les 11 cm
-    V_rd_values = V_rd(As_Es,fywd,d,cot_theta)
+    V_rd_values = V_rd(As_Es,fywd,d*10**-3,cot_theta)
     plot = plt.plot(t,V_rd_values,label="Résistance à l'effort tranchant")
     plt.xlabel("Temps [ans]")
     plt.ylabel("Résistance à l'effort tranchant [N]")
